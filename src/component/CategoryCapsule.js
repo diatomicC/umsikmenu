4c9b55a3caafd8f5683e8a4797488afada7253c4
mattmaomao <matthewcp220@gmail.com>
2024-08-10 21:01:13 +0800

    move filter function from capsule to slider

--- src/component/CategoryCapsule.js
+++ src/component/CategoryCapsule.js
@@ -1,37 +1,6 @@
 import { useEffect } from "react";
 
-export function CategoryCapsule(catText) {
-  useEffect(() => {
-    // todo
-    console.log("content: ", catText.content);
-  }, catText);
-
-  const selectCategory = (selected) => {
-    // clear selection
-    const allCat = document.querySelectorAll(".Selected");
-    var clearCategory = false;
-    allCat.forEach((cat) => {
-      if (cat.innerText === selected) {
-        clearCategory = true;
-      }
-      cat.classList.remove("Selected");
-    });
-
-    // enable the selected category
-    if (!clearCategory) {
-      const allCapsule = document.querySelectorAll(".CategoryCapsule");
-      for (let i = 0; i < allCapsule.length; i++) {
-        if (allCapsule[i].innerText === selected) {
-          allCapsule[i].classList.add("Selected");
-          break;
-        }
-      }
-    }
-    
-    // todo
-    // filter selected items
-  };
-
+export function CategoryCapsule({ catText, selectCategory }) {
   return (
     <button
       className="CategoryCapsule"
@@ -39,7 +8,7 @@ export function CategoryCapsule(catText) {
         selectCategory(catText.content);
       }}
     >
-      {catText.content}
+      {catText}
     </button>
   );
 }
--- src/component/CategorySlider.js
+++ src/component/CategorySlider.js
@@ -11,10 +11,42 @@ const CategorySlider = () => {
     setCategories(["category 1", "category 2", "category 3"]);
   }, []);
 
+  const selectCategory = (selected) => {
+    // clear selection
+    const allCat = document.querySelectorAll(".Selected");
+    var clearCategory = false;
+    allCat.forEach((cat) => {
+      if (cat.innerText === selected) {
+        clearCategory = true;
+      }
+      cat.classList.remove("Selected");
+    });
+
+    // enable the selected category
+    if (!clearCategory) {
+      const allCapsule = document.querySelectorAll(".CategoryCapsule");
+      for (let i = 0; i < allCapsule.length; i++) {
+        if (allCapsule[i].innerText === selected) {
+          allCapsule[i].classList.add("Selected");
+          break;
+        }
+      }
+    }
+
+    // todo
+    // filter selected items
+  };
+
   return (
     <div className="CategorySlider">
       {categories.map((cat, index) => {
-        return <CategoryCapsule key={index} content={cat} />;
+        return (
+          <CategoryCapsule
+            key={index}
+            catText={cat}
+            selectCategory={selectCategory}
+          />
+        );
       })}
     </div>
   );
