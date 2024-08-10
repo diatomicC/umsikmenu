361749aba76b712b76e7f87a8c920a7708c1aae1
mattmaomao <matthewcp220@gmail.com>
2024-08-10 21:07:55 +0800

    added title to banner, position the elementsw

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,10 +1,22 @@
-import React, { useEffect, useState } from "react";
-
 const Banner = () => {
   return (
-    <div style={{height: "120px", width: "100%", backgroundColor: "#0008"}}>
-      <img src="" alt="banner" />
-      <button style = {{position: "absolute", right: "0px"}}>lang</button>
+    <div
+      style={{
+        height: "80px",
+        width: "100%",
+        backgroundColor: "#0008",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img
+        src=""
+        alt="banner"
+        style={{ backgroundSize: "cover", backgroundPosition: "center" }}
+      />
+      <p style={{ position: "absolute", left: "25px" }}>Restaurant Name</p>
+      <button style={{ position: "absolute", right: "10px" }}>lang</button>
     </div>
   );
 };
